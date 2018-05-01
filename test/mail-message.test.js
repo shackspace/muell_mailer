06e6cccd4dc9e1bb415c8ceda6ccfdf431d37c3d
ulrich <git@myvdr.de>
2018-05-01 22:30:10 +0200

    URL in mail-message.js updated

--- test/mail-message.test.js
+++ test/mail-message.test.js
@@ -45,19 +45,19 @@ suite("mail-message", function() {
 				from: '"shack Muell Mailer Service " <xxxxxxxx@gmail.com>',
 				to: '"Mitgliedermailingliste shack e.V." <mitglieder@lists.shackspace.de>',
 				subject: '[SMMS] Gelber Sack wird morgen abgeholt',
-				text: 'Hallo shack,\n\nMorgen ( Freitag, 01. Januar 2016 ) wird der Gelber Sack abgeholt.\n\nBitte bringe den Müll nach unten und die Gelben Säcke die im Aufzugsvorraum stehen\nvor an die Ulmer Straße neben den Laternenmast!\n\nDieser Service wird präsentiert von:\n[SMMS] shack Muell Mailer Service [0]\n\n[0] http://shackspace.de/wiki/doku.php?id=muellabholung'
+				text: 'Hallo shack,\n\nMorgen ( Freitag, 01. Januar 2016 ) wird der Gelber Sack abgeholt.\n\nBitte bringe den Müll nach unten und die Gelben Säcke die im Aufzugsvorraum stehen\nvor an die Ulmer Straße neben den Laternenmast!\n\nDieser Service wird präsentiert von:\n[SMMS] shack Muell Mailer Service [0]\n\n[0] https://wiki.shackspace.de/infrastruktur/muellabholung'
 			}
 		);
 	});
 	test("check_output_gelber_sack_text", function() {
 		expect( mail_message.get_mail_options('gelber_sack', '2016-01-01').text ).to.deep.equal(
-				'Hallo shack,\n\nMorgen ( Freitag, 01. Januar 2016 ) wird der Gelber Sack abgeholt.\n\nBitte bringe den Müll nach unten und die Gelben Säcke die im Aufzugsvorraum stehen\nvor an die Ulmer Straße neben den Laternenmast!\n\nDieser Service wird präsentiert von:\n[SMMS] shack Muell Mailer Service [0]\n\n[0] http://shackspace.de/wiki/doku.php?id=muellabholung'
+				'Hallo shack,\n\nMorgen ( Freitag, 01. Januar 2016 ) wird der Gelber Sack abgeholt.\n\nBitte bringe den Müll nach unten und die Gelben Säcke die im Aufzugsvorraum stehen\nvor an die Ulmer Straße neben den Laternenmast!\n\nDieser Service wird präsentiert von:\n[SMMS] shack Muell Mailer Service [0]\n\n[0] https://wiki.shackspace.de/infrastruktur/muellabholung'
 		);
 		expect( mail_message.get_mail_options('restmuell', '2016-02-02').text ).to.deep.equal(
-				'Hallo shack,\n\nMorgen ( Dienstag, 02. Februar 2016 ) wird der Restmüll abgeholt.\n\nBitte bringe den Müll nach unten und lass die Mülltone aufgeschlossen!\n\nDieser Service wird präsentiert von:\n[SMMS] shack Muell Mailer Service [0]\n\n[0] http://shackspace.de/wiki/doku.php?id=muellabholung'
+				'Hallo shack,\n\nMorgen ( Dienstag, 02. Februar 2016 ) wird der Restmüll abgeholt.\n\nBitte bringe den Müll nach unten und lass die Mülltone aufgeschlossen!\n\nDieser Service wird präsentiert von:\n[SMMS] shack Muell Mailer Service [0]\n\n[0] https://wiki.shackspace.de/infrastruktur/muellabholung'
 		);
 		expect( mail_message.get_mail_options('papiermuell', '2016-03-03').text ).to.deep.equal(
-				'Hallo shack,\n\nMorgen ( Donnerstag, 03. März 2016 ) wird der Papiermüll abgeholt.\n\nBitte bringe den Müll nach unten und lass die Mülltone aufgeschlossen!\n\nDieser Service wird präsentiert von:\n[SMMS] shack Muell Mailer Service [0]\n\n[0] http://shackspace.de/wiki/doku.php?id=muellabholung'
+				'Hallo shack,\n\nMorgen ( Donnerstag, 03. März 2016 ) wird der Papiermüll abgeholt.\n\nBitte bringe den Müll nach unten und lass die Mülltone aufgeschlossen!\n\nDieser Service wird präsentiert von:\n[SMMS] shack Muell Mailer Service [0]\n\n[0] https://wiki.shackspace.de/infrastruktur/muellabholung'
 		);
 	});
 });
